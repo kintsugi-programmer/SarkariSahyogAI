72708a675472087db4cf34e35d594c8f0974e1ee
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-30 17:57:32 +0530

    Add Hero

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
-
+import { Hero } from "@/components/ui/Hero";
+import { Steps } from "@/components/ui/Steps";
 export default function Home() {
   return (
     <div>
-      
+      <section id="hero"><Hero/></section>
+      <section id ><Steps/></section>
+
     {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
