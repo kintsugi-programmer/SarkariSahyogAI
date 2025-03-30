032d39e9693588f2a3c5a9624acf7555a0e5bac3
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-30 16:11:11 +0530

    ShadCN

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,7 +2,9 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div>
+      
+    {/* <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -98,6 +100,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+    </div> */}
     </div>
   );
 }
