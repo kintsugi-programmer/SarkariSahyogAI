658af9f43189a42071495d125c2f58ea1bd6b51f
kintsugi-programmer <siddhant22496@iiitd.ac.in>
2025-03-30 17:57:52 +0530

    Modify DarkMode

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../../components/theme-provider";
+import Nav from "@/components/ui/Nav";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
+          <div className='w-full fixed left-0 z-50'>      <Nav/></div>
         {children}
         </ThemeProvider>
       </body>
